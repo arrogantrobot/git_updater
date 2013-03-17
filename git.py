e794548d92d760e788165ac1a192ff818a3f5dc9
arrogantrobot <arrogantrobot@gmail.com>
2013-03-17 11:34:54 -0500

    Fix merge

--- git.py
+++ git.py
@@ -3,10 +3,11 @@ import subprocess
 class git(object):
     def pull(self, daemonDir, remote, branch):
         cmd = ['git', 'pull', remote, branch]
+        print cmd
         p = subprocess.Popen(cmd, cwd=daemonDir)
         p.wait()
 
 if __name__ == "__main__":
     g = git()
-    g.pull("/home/rob/git/test", "origin", "master")
+    g.pull("/home/rob/git/python_git_updater", "origin", "master")
     print "This just updated itself."
